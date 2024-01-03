914fdf273290b79e67a67b8fc9014ff8dd440042
Arif Aydoğan <aydoganarif@yahoo.com>
2024-01-03 15:43:47 +0300

    AA

--- MetadataChangerApp/Services/FileService.cs
+++ MetadataChangerApp/Services/FileService.cs
@@ -4,26 +4,30 @@ namespace MetadataChangerApp.Services
 {
     public class FileService
     {
+        /// <summary>
+        /// Dosya ile ilgili meta bilgilerini okuma işlemleri burada yapılacak
+        /// </summary>
         public MetadataChangerApp.Models.FileInfo GetFileInfo(string filePath)
         {
             FileInfo fileInfo = new FileInfo(filePath);
 
-            // Dosya ile ilgili meta bilgilerini okuma işlemleri burada yapılacak
+            
             return new MetadataChangerApp.Models.FileInfo()
             {
                 FileName = Path.GetFileName(filePath),
                 CreationTime = fileInfo.CreationTime,
                 LastWriteTime = fileInfo.LastWriteTime
-                // Diğer meta bilgilerini ekleyin
             };
         }
 
+        /// <summary>
+        /// Seçilen dosyanın meta bilgilerini değiştirme işlemleri burada yapılacak
+        /// </summary>
         public void ChangeFileInfo(string filePath, MetadataChangerApp.Models.FileInfo newFileInfo)
         {
-            // Seçilen dosyanın meta bilgilerini değiştirme işlemleri burada yapılacak
+            
             File.SetCreationTime(filePath, newFileInfo.CreationTime);
             File.SetLastWriteTime(filePath, newFileInfo.LastWriteTime);
-            // Diğer meta bilgilerini güncelleme işlemleri
         }
     }
 }
